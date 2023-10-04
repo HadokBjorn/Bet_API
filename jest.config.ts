0a8a59113880455847469e2edb27531b89a4aedc
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-10-04 15:45:38 -0500

    :wrench: chore: configs to tests

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['<rootDir>/tests/**/*.(test|spec).ts'],
+  setupFiles: ['<rootDir>/tests/setup-envs.ts'],
+  //setupFilesAfterEnv: ['<rootDir>/tests/setup-files-after-env.ts'],
+ /*  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/tests/$1',
+    axios: 'axios/dist/node/axios.cjs',
+  }, */
+  restoreMocks: true,
+};
+
+export default config;
--- prisma/migrations/20231004172655_participants/migration.sql
+++ prisma/migrations/20231004172655_participants/migration.sql
@@ -1,10 +1,13 @@
 -- CreateTable
 CREATE TABLE "Participant" (
     "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-    "balance" BIGINT NOT NULL,
 
     CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participant_name_key" ON "Participant"("name");
