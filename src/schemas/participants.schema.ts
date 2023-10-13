816c1693c8bb94b6c077dbce1b1d1c66454c83c7
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-10-13 03:58:19 -0500

    :bug: fix: specified integer number in balance

--- src/schemas/participants.schema.ts
+++ src/schemas/participants.schema.ts
@@ -3,5 +3,5 @@ import { type ParticipantDTO } from '../utils'
 
 export const ParticipantSchema = joi.object<ParticipantDTO>({
   name: joi.string().min(3).trim().required(),
-  balance: joi.number().min(1000).required(),
+  balance: joi.number().integer().min(1000).required(),
 })
