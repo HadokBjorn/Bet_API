5ce50f200633a74dfecc2dc4c50681897df1d14e
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-10-12 22:56:48 -0500

    :sparkles: feat: games test

--- tests/integration/games.test.ts
+++ tests/integration/games.test.ts
@@ -1,11 +1,15 @@
 import app, { init } from '../../src/app'
 import supertest from 'supertest'
 import { cleanDB } from '../helpers'
-import { describe, it } from '@jest/globals'
+import { beforeAll, beforeEach, describe, it } from '@jest/globals'
 import httpStatus from 'http-status'
+import { prisma } from '../../src/config'
 
 beforeAll(async () => {
   await init()
+})
+
+beforeEach(async () => {
   await cleanDB()
 })
 const server = supertest(app)
@@ -45,5 +49,14 @@ describe('POST => /games', () => {
     expect(response.body[0]).toBe(
       '"awayTeamName" length must be at least 3 characters long',
     )
+  })
+
+  it('should respond with status 201 and create a game', async () => {
+    const game = { homeTeamName: 'Fluminense', awayTeamName: 'São Paulo' }
+    const response = await server.post('/games').send(game)
+    const isCreated = await prisma.game.findMany()
+
+    expect(response.status).toBe(httpStatus.CREATED)
+    expect(isCreated).toHaveLength(1)
   })
 })
